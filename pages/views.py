712c684769ad1f6b60472ba5aae007e9d235a82d
ing <inyoman_gurnitha@yahoo.com>
2020-08-18 16:26:44 +0700

    8/12 Added tempate to pages

--- pages/views.py
+++ pages/views.py
@@ -8,8 +8,10 @@ def home(request):
 def about(request):
 	return render(request, 'pages/about.html')
 
+
 def services(request):
 	return render(request, 'pages/services.html')
+
 
 def contact(request):
 	return render(request, 'pages/contact.html')
