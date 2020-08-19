cdb1895ae5c894836ef080da56769373cd454c75
ing <inyoman_gurnitha@yahoo.com>
2020-08-19 21:25:51 +0700

    23/30. Rendering Featured Cars

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from pages.models import Team
+from cars.models import Car 
 
 # Create your views here.
 def home(request):
 	teams = Team.objects.all()
+	# Get all featured cars
+	featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
 	data = {
 		'teams': teams,
-		"home_page": "active", # new info here
+		'featured_cars': featured_cars,
 	}
 	return render(request, 'pages/home.html', data)
 
