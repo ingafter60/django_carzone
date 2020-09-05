c34cd8d3255bbb5d62fd4126b7cabb6ef94c2a9d
ing <inyoman_gurnitha@yahoo.com>
2020-09-05 11:09:40 +0700

    54/62. Fix Login links in Registration Page

--- accounts/views.py
+++ accounts/views.py
@@ -164,7 +164,7 @@ def logout(request):
 
 	if request.method == 'POST':
 		auth.logout(request)
-		messages.success(request, 'You are successfully logged out!')
+		# messages.success(request, 'You are successfully logged out!')
 		return redirect('home')	
 	return redirect('home')	
 
